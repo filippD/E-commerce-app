d499a1814e3930549280a28cea247d14a4670f86
YellowPipe <philippdmitrienko@gmail.com>
2019-07-18 16:16:56 +0300

    added sign in/up page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shop/shop.component'
-import Header from './components/header/header.component'
-
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInUp from './pages/sign-in-up/sign-in-up.component'
 function App() {
   return (
     <div>
@@ -12,6 +12,7 @@ function App() {
       	<Switch>
       		<Route exact path='/' component={HomePage} />
       		<Route path='/shop' component={ShopPage} />
+      		<Route path='/signin' component={SignInUp} />
     	</Switch>
     </div>
   );
---
+++ src/pages/sign-in-up/sign-in-up.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './sign-in-up.styles.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SignInUp = () => (
+	<div cuplassName='sign-in-and-sign-'><SignIn /></div>
+)
+
+export default SignInUp;
