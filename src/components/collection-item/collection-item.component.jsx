024fa1a3b56d5d11b7401533a9a6c7456f2b20ef
YellowPipe <philippdmitrienko@gmail.com>
2019-07-21 14:56:36 +0300

    updated cart utils to group items in cart reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import './collection-item.styles.scss';
+import Button from '../button/button.component';
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+const CollectionItem = ({ item, addItem }) => {
+	const { name, price, imageUrl } = item;
+	return (
 	<div className='collection-item'>
 		<div
 			className='image'
@@ -13,7 +18,14 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
 			<span className='name'></span>
 			<span className='price'></span>
 		</div>
+		<Button onClick={()=>addItem(item)} inverted>
+			Add to cart
+		</Button>
 	</div>
-)
+)}
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+	addItem: item => dispatch(addItem(item))
+}) 
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
