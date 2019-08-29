3ba26939d5e4fb7f2e3a30b430f57ebadcda8812
YellowPipe <philippdmitrienko@gmail.com>
2019-08-29 16:11:13 +0300

    integrated stripe payments

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+	const priceForStripe = price*100;
+	const publishableKey = 'pk_test_Ku1dejvjNQJSWpetVh3nxg4N00ICAfykEV';
+	
+	const onToken = token => {
+		console.log(token);
+		alert('Payment Successful');
+	};
+
+	return (
+		<StripeCheckout
+			label='Pay Now'
+			name='CRWN Clothing Ltd.'
+			billingAddress
+			shippingAddress
+			image='https://svgshare.com/i/CUz.svg'
+			ammount={priceForStripe}
+			description={`Your total is $${price}`}
+			panelLabel='Pay Now'
+			token={onToken}
+			stripeKey={publishableKey}
+		/>
+	);
+};
+
+export default StripeCheckoutButton;
